e547634ae0acb38b2031a46e34d68c12804e59ed
Bastian Ilso <contact@bastianilso.com>
2019-03-20 09:03:52 +0100

    ArduinoLogger: Only log while in-game

--- assets/Scripts/ArduinoLogger.cs
+++ assets/Scripts/ArduinoLogger.cs
@@ -72,19 +72,19 @@ public class ArduinoLogger : MonoBehaviour
 
     void NewData(Arduino arduino)
     {
+        if (GameManager.GetInGame()) {
 
-        if (_MatchFittsLawLogging)
-        {
-            string changedSeperator = arduino.NewestIncomingData.Replace('\t', ';');
-            string date = DateTime.Now.ToString("yyyy-MM-dd");
-            string time = DateTime.Now.ToString("HH:mm:ss:ffff");
-            fileWriter.Write(date + ";" + time + ";" + changedSeperator);
-        } else {
-            fileWriter.Write((uint)(1000 * Time.realtimeSinceStartup) + "\t" + arduino.NewestIncomingData);
+            if (_MatchFittsLawLogging)
+            {
+                string changedSeperator = arduino.NewestIncomingData.Replace('\t', ';');
+                string date = DateTime.Now.ToString("yyyy-MM-dd");
+                string time = DateTime.Now.ToString("HH:mm:ss:ffff");
+                fileWriter.Write(date + ";" + time + ";" + changedSeperator);
+            } else {
+                fileWriter.Write((uint)(1000 * Time.realtimeSinceStartup) + "\t" + arduino.NewestIncomingData);
+            }
+
         }
-
-        
-        //fileWriter.Write(arduino.NewestIncomingData);
     }
 	
 	// Update is called once per frame
